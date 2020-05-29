99f49ec59260cbba6ad478cd7758bab1c1ba4004
DanielVanDeVelde <velde.van.de.daniel@gmail.com>
2020-05-29 14:36:33 +0200

    added more as well as a little diary

--- demos/js/ttltojson.js
+++ demos/js/ttltojson.js
@@ -7,14 +7,30 @@ fetch(
 
     const jsonld = ttl2jsonld.parse(ttl);
 
-    let array = new Array();
-    array = jsonld["@graph"].map(item => {
+    console.log(jsonld);
+
+    let array = jsonld["@graph"].map(item => {
       return {
         prefLabel: item["skos:prefLabel"],
-        broader: item["skos:broader"] ? item["skos:broader"] : false,
+        broader: item["skos:broader"]
+          ? sanitizeString(item["skos:broader"]["@id"])
+          : false,
         hiddenLabel: item["skos:hiddenLabel"] ? item["skos:hiddenLabel"] : false
       };
     });
-
     console.log(array);
   });
+
+function sanitizeString(parameter) {
+  let string = parameter;
+  string = string.match("([^/]+$)")[0]; //Grab everything behind the last "/"
+  string = string.replace(/_/g, " "); //Replace "_" with space
+  string = string.replace(/-/g, " "); //Replace "-" with space
+  string = string //Capitalize each word
+    .toLowerCase()
+    .split(" ")
+    .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+    .join(" ");
+
+  return string;
+}
